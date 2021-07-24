454f10dc97f4141f5961321ff8dcad4a9354e45f
Woo Hee, Chung <wooheemusic@gmail.com>
2021-07-24 09:44:47 +0900

    getQuadraticBezier.js, quadraticBezier named

--- src/explicits/getQuadraticBezier.js
+++ src/explicits/getQuadraticBezier.js
@@ -1,12 +1,13 @@
 
 import qb, { sdqb } from '../parametrics/quadraticBezier';
 import deduceFromParametric from '../tools/deduceFromParametric';
+import linear from './linear';
 
 function getInverse(xa) {
     if (sdqb(xa)() === 0) {
-        return x => x;
+        return linear;
     } else {
-        return function quadraticBezier(x) {
+        return function iqb(x) {
             return ((-2) * xa + Math.sqrt(4 * xa * xa + (4 - 8 * xa) * x)) / (2 - 4 * xa)
         }
     }
@@ -20,7 +21,7 @@ export function getNumericalQuadraticBezier(x, y) {
 export default function getQuadraticBezier(xa, ya) {
     let iqb = getInverse(xa);
     const qby = qb(ya);
-    return function (x) {
+    return function quadraticBezier(x) {
         return qby(iqb(x))
     }
 }
