dff4324bb10c4274ebbb67f63c325dc5f32f4d39
Woo Hee, Chung <wooheemusic@gmail.com>
2021-08-07 04:50:41 +0900

    useCurve.js, misc

--- src/useCurve.js
+++ src/useCurve.js
@@ -9,10 +9,10 @@ export default function useCurve(n = 60, fs = x => x, _options) {
     const range = useMemo(() => {
         if (typeof fs === 'function') return getRange(n, fs);
         if (Array.isArray(fs) && typeof fs[0] === 'function') return getSummedRange(n, ...fs);
-        throw new Error('useCurve requires the second argument of a function or a array of functions')
+        throw new Error('useCurve requires the second argument of a function or an array of functions')
     }, []);
-    const options = useMemo(()=>{ 
-        const { range: _, ...rest } = _options; // range in _options is ignored.
+    const options = useMemo(() => {
+        const { range: _, ...rest } = (typeof _options === 'function' ? _options() : _options) || {}; // range in _options is ignored.
         return { range, ...rest }
     }, [])
     return useFrames(options);
