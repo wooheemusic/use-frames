0e02d7473c223e5dc6bf2c3ed65b28b18439dd52
Woo Hee, Chung <wooheemusic@gmail.com>
2021-08-09 01:02:59 +0900

    misc

--- src/createUseSpring.js
+++ src/createUseSpring.js
@@ -6,7 +6,6 @@ function distance(a, b) {
 }
 
 /**
- * 
  * @param {number|Object|Array} [initState = 1] 1, 2, 3, ... or an array like [{ v: 0, d: 0 }, { v: 0, d: 0 }, ...]
  * @param {Object|Function} [_options] {stiffness: 100, mass: 1, damping: 5, frequency = 60, precision: 0.0001}
  * @returns {Function} a react hook
@@ -17,10 +16,10 @@ export default function createUseSpring(initState, options) {
         const [H, setH, isValid] = useDynamicHarmonic(initState, options);
         const isHArray = Array.isArray(H);
         const [R, setR] = useState(C);
-        useEffect(() => setH(C.map((el, i) => ({ d: distance(el, R[i]), v: isHArray ? H[i].v : H.v, stiffness, damping, mass }))), C)
+        useEffect(() => setH(C.map((el, i) => ({ d: distance(el, R[i]), v: (isHArray ? H[i].v : H.v), stiffness, damping, mass }))), C)
         useEffect(() => {
-            isValid && setR(C.map((el, i) => distance(el, isHArray ? H[i].d : H.d)));
-        }, isHArray ? H.map(el => el.d) : [H.d])
+            isValid && setR(C.map((el, i) => distance(el, (isHArray ? H[i].d : H.d))));
+        }, (isHArray ? H.map(el => el.d) : [H.d]))
         return [...R, setSpring];
     }
 }
